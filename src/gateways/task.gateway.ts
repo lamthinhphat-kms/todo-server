20157500c5dc4dbf3404738644bf7058bef16fa1
Phat Lam <phatlam@kms.com.vn>
2023-09-06 14:52:50 +0700

    fix socket

--- src/gateways/task.gateway.ts
+++ src/gateways/task.gateway.ts
@@ -4,7 +4,7 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { TaskService } from 'src/task/task.service';
 
 @WebSocketGateway(8001, {
@@ -16,12 +16,23 @@ export class TaskGateway {
   constructor(private taskService: TaskService) {}
   @WebSocketServer()
   server: Server;
+
   @SubscribeMessage('task')
   async handleTaskUpdate(@MessageBody('userId') userId: number): Promise<void> {
-    console.log(userId);
     this.server
       .to(`task - ${userId}`)
       .emit('task', await this.taskService.findAllTask(userId));
     // this.server.emit('task', await this.taskService.findAllTask(userId));
+  }
+
+  @SubscribeMessage('listen')
+  async handleListenToTask(
+    @MessageBody('sub') userId: number,
+    @MessageBody('socketId') socketId: string,
+  ): Promise<void> {
+    console.log(userId);
+    if (socketId) {
+      await this.server.in(socketId).socketsJoin(`task - ${userId}`);
+    }
   }
 }
